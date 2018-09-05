0110d504b4cc7ae96d04d4bae9d15708239b2043
Ryan Geary <rtgnj42@gmail.com>
2018-09-05 10:26:25 -0400

    get file names

--- mycp.c
+++ mycp.c
@@ -9,14 +9,32 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 
 #include "mycp.h"
 
+// printUsage - print an explanatory error message to help users
 void printUsage() {
   printf( "\nUsage:\nmycp source_file destination_file\n");
 }
 
 int main(int argc, char* argv[]) {
+  char source_file_name[128];
+  char dest_file_name[128];
+  FILE source_file;
+  FILE dest_file;
+
+  if(argc == 3) {
+    //get file names
+    strcpy(source_file_name, argv[1]);
+    strcpy(dest_file_name, argv[2]);
+
+    
+  }
+  else {
+    printUsage();
+  }
 
 
   return 0;
