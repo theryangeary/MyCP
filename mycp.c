80087634068d6f74eecc21683a177916f1a7b442
Ryan Geary <rtgnj42@gmail.com>
2018-09-05 10:06:43 -0400

    add summary, includes, and main

---
+++ mycp.c
@@ -0,0 +1,18 @@
+/*
+  mycp - copy one file's contents to another
+  Ryan Geary - September 5, 2018
+  CISC361 Fall 2018
+  Programming Assignment 1b
+*/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include "mycp.h"
+
+int main(int argc, char* argv[]) {
+
+  return 0;
+}
